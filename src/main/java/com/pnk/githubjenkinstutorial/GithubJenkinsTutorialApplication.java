18b9817557295abdc6a7db07a4626caec6442f54
Phong Vo <vhphong@gmail.com>
2024-01-12 16:44:17 -0600

    Add existing project files to Git

---
+++ src/main/java/com/pnk/githubjenkinstutorial/GithubJenkinsTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.pnk.githubjenkinstutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubJenkinsTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubJenkinsTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pnk/githubjenkinstutorial/GithubJenkinsTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnk.githubjenkinstutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubJenkinsTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
