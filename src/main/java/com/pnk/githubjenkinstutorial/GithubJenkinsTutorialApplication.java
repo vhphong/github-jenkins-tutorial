1b9a55e4cf0baa29620507a8c7805487e7d2856f
Phong Vo <vhphong@gmail.com>
2024-01-13 17:50:36 -0600

    an error in code

--- src/main/java/com/pnk/githubjenkinstutorial/GithubJenkinsTutorialApplication.java
+++ src/main/java/com/pnk/githubjenkinstutorial/GithubJenkinsTutorialApplication.java
@@ -19,7 +19,7 @@ public class GithubJenkinsTutorialApplication {
     }
 
 
-    public static void main(String[] args) {
+    public static void /*main*/(String[] args) {
         logger.info("Application started..");
         SpringApplication.run(GithubJenkinsTutorialApplication.class, args);
     }
